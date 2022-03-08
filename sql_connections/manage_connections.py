aa6c3c8fb07856f8bb2793b98094ef31965acf77
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2022-03-08 10:01:39 -0800

    began SQL setup

---
+++ sql_connections/manage_connections.py
@@ -0,0 +1,20 @@
+from psycopg import OperationalError, connect
+import os
+
+
+def create_connection():
+    try:
+        conn = connect(
+            host=os.environ.get("HOST"),
+            dbname=os.environ.get("DBNAME"),
+            user=os.environ.get("USER"),
+            password=os.environ.get("PASSWORD"),
+            port=os.environ.get("PORT")
+        )
+        return conn
+    except OperationalError as exception:
+        print(exception)
+
+connection = create_connection()
+
+print(connection)
